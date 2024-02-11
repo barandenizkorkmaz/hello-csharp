efb078e544c1738fb11cbcfd89028111ff9fa52c
barandenizkorkmaz <barandenizkorkmaz@gmail.com>
2024-02-11 20:55:45 +0100

    feat: ✨ add sections

---
+++ src/S18LINQ/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace S18LINQ;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, S18!");
+    }
+}
---
+++ src/S22Testing/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace S22Testing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
